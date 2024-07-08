0f6beacd6e3eaf00b7ce6cf128ae730ee4598efd
Raisho Alam <157357606+raishoalam@users.noreply.github.com>
2024-07-08 13:03:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import {
+  HeaderContainer,
+  HeaderElementsContainer,
+  Logo,
+} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderElementsContainer>
+      <Logo
+        src="https://img.freepik.com/premium-vector/illustration-post_498740-20309.jpg"
+        alt="nxt player logo"
+      />
+    </HeaderElementsContainer>
+  </HeaderContainer>
+)
+
+export default Header
